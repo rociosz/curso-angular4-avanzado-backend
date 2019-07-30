9e5ab56dc6e901c8d7fd0313195f86c31ccab3d3
rociosz <rocio.sz.29@gmail.com>
2019-07-30 17:26:19 -0500

    Sección 13 - Acción para el Registro de Usuarios

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,60 @@
 'use strict'
 
+// Modulos
+var bcrypt = require('bcrypt-nodejs');
 
+// Modelos
+var User = require('../models/user');
+
+// Acciones
 function pruebas(req, res){
     res.status(200).send({
         message: 'Probando el controlador de usuarios y la acción pruebas',
     });
 }
 
+function saveUser(req, res){
+
+    // Crear objeto usuario
+    var user = new User();
+
+    // Recoger parametros peticion
+    var params = req.body;
+
+    if(params.password && params.name && params.email){
+
+        // Asignar valores al objeto de usuario
+        user.name = params.name;
+        user.surname = params.surname;
+        user.email = params.email;
+        user.role = 'ROLE_USER';
+        user.image = null;
+
+        bcrypt.hash(params.password, null, null, function(err, hash){
+            user.password = hash;
+
+            // Guardar usuario en bd
+            user.save((err, userStored) => {
+                if(err){
+                    res.status(500).send({message: 'Error al guardar el usuario'}); 
+                }else{
+                    if(!userStored){
+                        res.status(404).send({message: 'No se ha registrado el usuario'});
+                    }else{
+                        res.status(200).send({user: userStored});
+                    }
+                }
+            });
+        });
+        
+    }else{
+        res.status(200).send({
+            message: 'Introduce los datos correctamnte para poder registrar al usuario'
+        });
+    }
+}
+
 module.exports = {
-    pruebas
+    pruebas,
+    saveUser
 };
--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,6 @@ var UserContoller = require('../controllers/user');
 var api = express.Router();
 
 api.get('/pruebas-del-controlador', UserContoller.pruebas);
+api.post('/register', UserContoller.saveUser);
 
 module.exports = api;
